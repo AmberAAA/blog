50f56a63900bf46b7dafa9cfccfec21a8fe7fc51
AmberAAA <anborong@suanier.com>
2021-03-04 12:39:20 +0800

    feat: 消失的十三级台阶

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,10 +6,10 @@ module.exports = {
       { text: "首页", link: "/" },
       { text: "食谱", link: "/menu/" },
       { text: "代码", link: "/" },
-      { text: "杂文", link: "/" },
+      { text: "杂文", link: "/note/" },
       { text: "年终总结", link: "/" },
     ],
-    sidebar: ["/", "/menu/"],
+    sidebar: ["/", "/menu/", "/note/"],
     // 假定 GitHub。也可以是一个完整的 GitLab 网址
     repo: "amberaaa/blog",
     docsDir: "docs",
