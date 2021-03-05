1c80b8fc064bd54c37be22e111672e4024ad92c4
AmberAAA <anborong@suanier.com>
2021-03-05 12:55:07 +0800

    百度

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "Amber代码屋",
   description: "Amber代码屋 | 分享程序员知识与生活的平台",
-  header: [
+  head: [
     ["meta", { name: "baidu-site-verification", content: "code-BIOahWpBKG" }],
   ],
   themeConfig: {
