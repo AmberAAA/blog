672d7011134ca68682ec8641a55f816759642320
Amber <anborong1314@gmail.com>
2023-05-31 12:12:51 +0800

    chore(other): change dumi to docusaurus

--- docs/store/README.md
+++
@@ -1,9 +0,0 @@
----
-title: 归档
-order: 1
-nav:
-	title: 归档
-	order: 999
----
-
-# 归档
--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
 npm run build
-scp -r ./dist/ root@anborong.top:~/blog/
+scp -r ./build/ root@anborong.top:~/blog/
 #open http://blog.anborong.top
