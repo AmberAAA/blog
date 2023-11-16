cc92dcd80e55efdce6c2c09401fce690484af6fb
AmberAAA <anborong1314@gmail.com>
2023-11-16 16:15:57 +0800

    init up v3

--- docs/react/react-optimization.md
+++ docs/react/react-optimization.md
@@ -18,14 +18,8 @@ nav:
 1. `memo`是一个高阶函数，第一个参数接收一个 React Dom，第二个参数，传入一个比较函数，判断是否需要更新组件。
 2. 使用`useMemo`缓存变量，以及使用`useCallBack`缓存函数，再通过`memo`控制 React Dom 的 render 时机
 
-## 实例
-
-:::info{title=自定义标题}
-
-1.请打开控制台查看输出
-
-2.请打开源码查看备注
-
-:::
-
-<code src="../../src/components/optimization/index.tsx"> </code>
+```jsx live
+function HelloCodeTitle(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require('prism-react-renderer')
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -115,8 +116,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} MIT.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: lightTheme,
+        darkTheme: darkTheme,
       },
       algolia: {
         appId: 'GJ6H7GCAJO',
